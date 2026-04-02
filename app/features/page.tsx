df3491b10bad29b440e5f3ecd4d033f3ee3fba24
Navod T Wijesooriya <navodtwijesooriya@gmail.com>
2026-04-02 14:04:02 +0530

    link nav abd footer

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Features from '@/components/features/Features'
+import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
     <div>
+      <Navbar />
       <Features />
+      <Footer />
 
     </div>
   )
